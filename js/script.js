2f3e510190ed7c06ada84b4b52fb663556563445
Kalgo <kalgothka_4444@protonmail.com>
2025-03-23 00:48:34 +0500

    add header

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const list = document.querySelector(".navbar__collaction")
+const topEl = document.querySelector(".top")
+const headerEl = document.querySelector(".header")
+
+window.addEventListener("scroll", ()=>{
+    let offset = document.documentElement.scrollTop
+    if(offset > 200){
+        topEl.classList.remove("hidden")
+    }else{
+        topEl.classList.add("hidden")
+    }
+
+    if(offset > 0){
+        headerEl.classList.add("shrink")
+    }else{
+        headerEl.classList.remove("shrink")
+    }
+})
+
+function toggleNavbar(){
+    list.classList.toggle("showBurger")
+}
